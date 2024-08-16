a0e3122d74640773f0fe6a7ba1c4c75504ab098e
DasLampe <daslampe@lano-crew.org>
2024-08-16 17:45:14 +0200

    feat: add password for bgp

--- metadata.py
+++ metadata.py
@@ -7,3 +7,21 @@ def set_router_id(metadata):
             'router_id': metadata.get('bird/router_id', main_interface_ip)
         }
     }
+@metadata_reactor
+def set_default_bgp_password(metadata):
+    return_dict = {
+        'bird': {
+            'protocols': {
+                'bgp': {},
+            }
+        }
+    }
+
+    local_as = metadata.get('bird/as')
+    for name, cfg in metadata.get('bird/protocols/bgp', {}).items():
+        return_dict['bird']['protocols']['bgp'][name] = {
+            'password': repo.vault.password_for("bgp_password_{0}_to_{1}".format(*sorted([int(local_as), int(cfg.get('as'))])))
+        }
+
+    return return_dict
+
